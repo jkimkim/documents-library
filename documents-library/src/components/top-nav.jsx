383e7458ac64f85019c61f15448dfe906ff6976f
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-03-09 10:50:37 +0300

    update

--- documents-library/src/components/top-nav.jsx
+++ documents-library/src/components/top-nav.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import 'halfmoon/css/halfmoon-variables.min.css';
-import 'halfmoon/css/halfmoon.min.css';
+// import 'halfmoon/css/halfmoon-variables.min.css';
+// import 'halfmoon/css/halfmoon.min.css';
 
 
 import './css/top-nav.scss';
