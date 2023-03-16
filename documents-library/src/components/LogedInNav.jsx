23d1f2e9f4a86ad1fc386ccbe73e7b2701747082
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-03-16 19:27:00 +0300

    modified:   documents-library/src/components/LogedInNav.jsx

--- documents-library/src/components/LogedInNav.jsx
+++ documents-library/src/components/LogedInNav.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'halfmoon/css/halfmoon-variables.min.css';
+import 'halfmoon/css/halfmoon.min.css';
+import 'halfmoon/js/halfmoon.min.js'
 import './css/top-nav.scss';
 
 class logedInNav extends Component {
@@ -15,54 +16,47 @@ class logedInNav extends Component {
 
     }
     render() {
+        const halfmoon = window.halfmoon;
         return (
             <React.Fragment>
                 {/* Top Nav */}
 
-                <header className="flex flex-row pt-1 pb-1">
-                    <div
-                        className="logo fw-bold rounded"
-                        onClick={() => {
-                            window.location = '/';
-                        }}
-                    >
-                        <div
-                            className="logo mx-1 rounded"
-                            style={{
-                                backgroundImage: `url(${this.state.user.image})`,
-                                backgroundSize: 'cover',
-                                backgroundPosition: 'center',
-                                backgroundRepeat: 'no-repeat',
-                                width: '40px',
-                                height: '40px',
-                            }}
-                        ></div>
-                        DocsLib
-                    </div>
-                    {/* search bar */}
-                    <div className="search-bar">
-                        <input type="text" placeholder="Search" />
-                    </div>
-                    {/* User profile */}
-                    <div className="user-profile flx">
-                        <div className="user-profile-image">
-                            <div
-                                className="user-profile-image rounded"
-                                style={{
-                                    backgroundImage: `url(${this.state.user.image})`,
-                                    backgroundSize: 'cover',
-                                    backgroundPosition: 'center',
-                                    backgroundRepeat: 'no-repeat',
-                                    width: '40px',
-                                    height: '40px',
-                                }}
-                            ></div>
+                <div className="page-wrapper with-navbar with-sidebar">
+                    <nav className="navbar">
+                        <div className='navbar-content'>
+                            <button id='toggle-sidebar-btn' className='btn btn-action btn-link' type='button' onClick={
+                                () => {
+                                    halfmoon.toggleSidebar();
+                                }
+                            }>
+                                <i className='fa fa-bars' aria-hidden='true'></i>
+                            </button>
                         </div>
-                        <div className="user-profile-name">
-                            <p>{this.state.user.name}</p>
+                        <a href='#' className='navbar-brand ml-10 ml-sm-20'>
+                            <img src='https://www.techno.com/wp-content/uploads/2019/03/techno-logo.png' alt='DocsLib' />
+                            <span className='d-none d-sm-flex'>
+                                DocsLib
+                            </span>
+                        </a>
+
+                        {/* profile */}
+                        <div className='navbar-content ml-auto'>
+                            <div className='dropdown with-arrow'>
+                                <button className='btn btn-action btn-link' id='profileDropdownMenuLink' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>
+                                    <i className='fa fa-user' aria-hidden='true'></i>
+                                </button>
+                                <div className='dropdown-menu dropdown-menu-right' aria-labelledby='profileDropdownMenuLink'>
+                                    <a className='dropdown-item' href='#'>My Profile</a>
+                                    <a className='dropdown-item' href='#'>My Docs</a>
+                                    <a className='dropdown-item' href='#'>Upload</a>
+                                    <div className='dropdown-divider'></div>
+                                    <a className='dropdown-item' href='#'>Log Out</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </header>
+                    </nav>
+                </div>
+                
 
                 {/* Side Nav */}
 
