827ffae09e2c48035fed13eb57280c8490585340
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-03-17 20:55:31 +0300

    modified:   documents-library/src/components/Loader.js 	new file:   documents-library/src/components/css/loader.css

--- documents-library/src/components/Loader.js
+++ documents-library/src/components/Loader.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ' ./Bootstrap.css '
-import ' ./Loader.css'
+import ' ./css/Loader.css'
 
 function Loader() {
   return (
-    <div>Loader</div>
+    <div className="loader">
+      <div className="spinner-border text-primary" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
   )
 }
 
