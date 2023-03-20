dd562fad754d0289e364dd9d37da8942cc17e13e
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-03-20 12:04:25 +0300

    modified:   documents-library/src/screens/Home.js

--- documents-library/src/screens/Home.js
+++ documents-library/src/screens/Home.js
@@ -1,11 +1,20 @@
 import AuthProvider from '../Context/AuthContext';
 import LogedInNav from '../components/LogedInNav';
 import MostPopularReads from '../components/MostPopularReads';
+import {
+    Container,
+} from 'react-bootstrap'
 
 function LoggedIn() {
     return (
         <AuthProvider>
-            <LogedInNav />
+            <div className="d-flex flex-row " style={{ minHeight: "100vh" }}>
+                <LogedInNav />
+                {/* body */}
+                <Container className="d-flex flex-column">
+                    <MostPopularReads />
+                </Container>
+            </div>
         </AuthProvider>
     );
 }
