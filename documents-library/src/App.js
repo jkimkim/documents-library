4898a4b0b8b2f488bc84007fe74313d386064515
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-04-15 00:01:00 +0300

    update

--- documents-library/src/App.js
+++ documents-library/src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import LoggedOut from './screens/LoggedOut';
 import LoggedIn from './screens/Home';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
